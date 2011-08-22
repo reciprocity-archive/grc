a33fe2c0f35f9d529f0c269d5086c757a54f5176
Miron Cuperman <miron+cms@google.com>
2011-08-22 15:40:16 -0700

    blank rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CmsRails::Application.config.secret_token = '2e0d6a7682ac5f5a9a0868f0fa8c389516a7d4eee7e5b08400d79e33e80a4cd728c8cf073b02fb9938de2341866ba834a6ea26973a0cb61613c491ad98d124ab'
