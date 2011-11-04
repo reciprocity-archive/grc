d77976bd28c6abc8e1c291a8469e8ff514e7feb8
Miron Cuperman <miron+cms@google.com>
2011-11-04 15:39:39 -0700

    Document ApplicationController, add license doc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
+# Author:: Miron Cuperman (mailto:miron+cms@google.com)
+# Copyright:: Google Inc. 2011
+# License:: Apache 2.0
+
+# This class is the base for all controllers used in this app
+
 require 'dm-rails/middleware/identity_map'
 class ApplicationController < ActionController::Base
   before_filter :require_user
@@ -8,6 +14,7 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user_session, :current_user
 
+  # By default allow only admin access.  This is relaxed in specific controllers.
   access_control :acl do
     allow :admin
   end
@@ -19,52 +26,62 @@ class ApplicationController < ActionController::Base
   end
 
   private
-    def current_user_session
-      return @current_user_session if defined?(@current_user_session)
-      @current_user_session = UserSession.find
-    end
-    
-    def current_user
-      return @current_user if defined?(@current_user)
-      @current_user = current_user_session && current_user_session.record
-    end
-    
-    def require_user
-      unless current_user
-        store_location
-        flash[:notice] = "You must be logged in to access this page"
-        redirect_to login_url
-        return false
-      end
-    end
 
-    def require_no_user
-      if current_user
-        store_location
-        flash[:notice] = "You must be logged out to access this page"
-        redirect_to root_url
-        return false
-      end
-    end
-    
-    def store_location
-      session[:return_to] = request.fullpath
-    end
-    
-    def redirect_back_or_default(default)
-      redirect_to(session[:return_to] || default)
-      session[:return_to] = nil
-    end
+  # The current user session or nil
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
 
-    def regulation_filter_set
-      regulation_id = params[:regulation][:id] rescue nil
-      if !regulation_id.nil?
-        if regulation_id == ""
-          session[:regulation_id] = nil
-        else
-          session[:regulation_id] = regulation_id
-        end
+  # The current user or nil
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.record
+  end
+
+  # Pre-filter for requiring the user to be logged in.  On by default.
+  def require_user
+    unless current_user
+      store_location
+      flash[:notice] = "You must be logged in to access this page"
+      redirect_to login_url
+      return false
+    end
+  end
+
+  # Pre-filter for requiring the user to not be logged in
+  def require_no_user
+    if current_user
+      store_location
+      flash[:notice] = "You must be logged out to access this page"
+      redirect_to root_url
+      return false
+    end
+  end
+
+  # Memoize current request location so that we can return to it after a redirect
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+
+  # Redirect back to memoized location
+  def redirect_back_or_default(default)
+    redirect_to(session[:return_to] || default)
+    session[:return_to] = nil
+  end
+
+  # Set the regulation to filte rby if the user selected one on previous page.  This is
+  # a before filter that is always on, since the regulation filter pulldown appears on
+  # many pages.
+  def regulation_filter_set
+    regulation_id = params[:regulation][:id] rescue nil
+    if !regulation_id.nil?
+      if regulation_id == ""
+        session[:regulation_id] = nil
+      else
+        session[:regulation_id] = regulation_id
       end
     end
+  end
 
 end
