b926c1d9527b6d830cd9566c77505902c41b53ea
devrandom1 <c1.github@niftybox.net>
2012-02-29 17:21:00 -0800

    Change cookie names to prevent hijack of existing sessions.
    You might need Account.forget_all! to reset credential tokens.

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -2,4 +2,5 @@
 class UserSession < Authlogic::Session::Base
   extend ActiveModel::Naming
   authenticate_with Account
+  cookie_key "_cms_cred"
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,4 +3,4 @@
 require 'dm-rails/session_store'
 
 # Store sessions in the database, managed by Datamapper
-CmsRails::Application.config.session_store Rails::DataMapper::SessionStore, :secure => Rails.env.production?
+CmsRails::Application.config.session_store Rails::DataMapper::SessionStore, :secure => Rails.env.production?, :key => '_cms_session_id'
