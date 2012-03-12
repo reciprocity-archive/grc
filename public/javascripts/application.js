5cde68938fdab378c1cffdc6dfa0df20979f92b2
Miron Cuperman <miron+code@google.com>
2012-03-12 15:25:19 -0700

    Expandable document section for systems on evidence page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,9 @@
 // Put your application scripts here
+jQuery(document).ready(function(){
+  $('.collapsible .head').click(function() {
+      $(this).toggleClass('toggle');
+      $(this).next().toggle();
+      return false;
+  }).next().hide();
+});
+
