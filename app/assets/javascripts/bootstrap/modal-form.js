94535e4cde39a7692a8b2b428d75f54d77a8ae4d
Dan Ring <danring@google.com>
2012-06-12 15:31:32 -0700

    Fix $.fn.modal_form plugin

--- app/assets/javascripts/bootstrap/modal-form.js
+++ app/assets/javascripts/bootstrap/modal-form.js
@@ -13,11 +13,8 @@
     this.$element
       .on('click.modal-form.close', '[data-dismiss="modal"]', $.proxy(this.hide, this))
       .on('click.modal-form.reset', 'input[type=reset], [data-dismiss="modal-reset"]', $.proxy(this.reset, this))
-  //    .on('click.modal-form.submit', 'input[type=submit], [data-dismiss="modal-submit"]', $.proxy(this.submit, this));
-      .on('ajax:success',  'form', $.proxy(this.on_success, this))
-      .on('ajax:error',    'form', $.proxy(this.on_error, this))
-      .on('flash:add',     $.proxy(this.on_flash, this))
-      //.on('ajax:complete', 'form', $.proxy(this.on_complete, this))
+      .on('ajax:complete', 'form', $.proxy(this.on_complete, this))
+      .on('ajax:flash',    'form', $.proxy(this.on_flash, this))
   }
 
   /* NOTE: MODAL_FORM EXTENDS BOOTSTRAP-MODAL.js
@@ -28,7 +25,6 @@
     constructor: ModalForm
 
   , reset: function(e) {
-      console.debug("reset");
       $(e.target).closest('form')[0].reset();
       this.hide(e);
     }
@@ -38,27 +34,60 @@
       this.$element.off('modal_form');
     }
 
-  , submit: function(e) {
-      var $form = $(e.target);
-      e && e.preventDefault();
-    }
+  , on_complete: function(ev, xhr, status) {
+      // Replace form body
+      this.$element.find('.modal-body').html(xhr.responseText);
 
-  , on_success: function(ev, data, status, xhr) {
-      // Hide, then display flash messages globally
-      if (data.redirect) {
-        window.location.replace(data.redirect);
-      } else {
-        this.hide();
-        this.$element.trigger('flash', status);
+      // Maybe handle AJAX/JSON redirect or refresh
+      if (xhr.status == 278) {
+        // Handle 278 redirect (AJAX redirect)
+        window.location.assign(xhr.getResponseHeader('location'));
+      } else if (xhr.status == 279) {
+        window.location.assign(window.location.href);
       }
+
+      // Maybe handle AJAX flash messages
+      var flash_types = ["error", "alert", "notice", "warning"]
+        , type_i, message
+        , flash;
+
+      for (type_i in flash_types) {
+        message = xhr.getResponseHeader('x-flash-' + flash_types[type_i]);
+        message = JSON.parse(message);
+        if (message) {
+          if (!flash)
+            flash = {};
+          flash[flash_types[type_i]] = message;
+        }
+      }
+
+      if (flash)
+        this.$element.find('form').trigger('ajax:flash', flash);
     }
 
-  , on_error: function(xhr, status, error) {
-      // Display flash messages
-      this.$element.trigger('flash', error);
-    }
+  , on_flash: function(e, flash) {
+      // Find or create the flash-message holder
+      var $flash_holder = this.$element.find('.flash')
+        , type, ucase_type
+        , messages, message, message_i;
 
-  , on_flash: function(e, messages) {
+      if ($flash_holder.length == 0) {
+        $flash_holder = $('<ul class="flash"></ul>');
+        this.$element.find('.modal-body').prepend($flash_holder);
+      } else {
+        $flash_holder.empty();
+      }
+
+      for (type in flash) {
+        if (flash[type]) {
+          if (typeof(flash[type]) == "string")
+            flash[type] = [flash[type]];
+
+          for (message_i in flash[type]) {
+            $flash_holder.append('<li class="' + type + '">' + flash[type][message_i] + '</li>');
+          }
+        }
+      }
       e.stopPropagation();
     }
   });
@@ -84,10 +113,10 @@
    * =================== */
 
   $(function () {
-    $('body').on('click.modal.data-api', '[data-toggle="modal-form"]', function ( e ) {
+    $('body').on('click.modal-form.data-api', '[data-toggle="modal-form"]', function ( e ) {
       var $this = $(this), href
         , $target = $($this.attr('data-target') || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '')) //strip for ie7
-        , option = $target.data('modal') ? 'toggle' : $.extend({}, $target.data(), $this.data());
+        , option = $target.data('modal-form') ? 'toggle' : $.extend({}, $target.data(), $this.data());
 
       e.preventDefault();
       $target.modal_form(option);
