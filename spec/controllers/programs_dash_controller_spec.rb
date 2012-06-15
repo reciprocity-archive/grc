08d3db8fefd8ce158548e2a131834a09474d0640
Miron Cuperman <miron+code@google.com>
2012-06-15 16:27:42 -0700

    Fix some tests

--- spec/controllers/programs_dash_controller_spec.rb
+++ spec/controllers/programs_dash_controller_spec.rb
@@ -17,8 +17,8 @@ describe ProgramsDashController do
     before :each do
       login({}, { :role => 'admin' })
       create_base_objects
-      @ctl2 = Control.create(:title => 'Control 2', :slug => 'REG1-CTL2', :description => 'x', :is_key => true, :fraud_related => false)
-      @ctl3 = Control.create(:title => 'Control 3', :slug => 'REG1-CTL2-3', :description => 'x', :is_key => true, :fraud_related => false)
+      @ctl2 = Control.create(:title => 'Control 2', :slug => 'REG1-CTL2', :description => 'x', :is_key => true, :fraud_related => false, :program => @creg)
+      @ctl3 = Control.create(:title => 'Control 3', :slug => 'REG1-CTL2-3', :description => 'x', :is_key => true, :fraud_related => false, :program => @creg)
       @sec2 = Section.create(:title => 'Section 2', :slug => 'REG1-SEC2', :description => 'x', :program => @reg)
       @sec3 = Section.create(:title => 'Section 3', :slug => 'REG1-SEC3', :description => 'x', :program => @reg)
       @sec2.controls << @ctl
