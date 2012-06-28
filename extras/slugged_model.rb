67e1a98f664877dc3194b0efde39b2b64889fe3d
Dan Ring <danring@google.com>
2012-06-27 20:33:09 -0700

    Refactor section form; fix styles for multi-error fields; fix nil error with slugged_model mixin; use 'modal-reset' on ajax forms

--- extras/slugged_model.rb
+++ extras/slugged_model.rb
@@ -83,10 +83,10 @@ private
   def validate_slug
     upcase_slug
     return unless parent
-    if slug.start_with?(parent.slug)
+    if slug && slug.starts_with?(parent.slug)
       true
     else
-      errors.add(:slug, "control slug must start with parent's slug #{parent.slug}")
+      errors.add(:slug, "must start with parent's code #{parent.slug}")
     end
   end
 end
