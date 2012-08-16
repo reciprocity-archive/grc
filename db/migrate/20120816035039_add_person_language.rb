9addd81a1471a8eb87a0b2f97926cd17543cfe37
Dan Ring <danring@google.com>
2012-08-16 10:08:10 -0700

    Add language_id to Person model

---
+++ db/migrate/20120816035039_add_person_language.rb
@@ -0,0 +1,19 @@
+class AddPersonLanguage < ActiveRecord::Migration
+  # Add a 'language_id' association from Person to the Option table
+
+  # Local models so that migration is safe
+  class Option < ActiveRecord::Base
+  end
+
+  def up
+    add_column :people, :language_id, :integer
+
+    Option.reset_column_information
+
+    Option.create(:role => 'person_language', :title => 'English')
+  end
+
+  def down
+    remove_column :people, :language_id
+  end
+end
