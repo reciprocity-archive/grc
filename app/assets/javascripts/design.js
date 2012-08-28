3eade9c97acc09f2babecb29cfccf0fc73065081
Joseph Van Geffen <geffen@gmail.com>
2012-08-28 15:40:00 -0700

    Modal footers now in a partial, initial work on GRC menu in dash, pivotal modal fixes.

--- app/assets/javascripts/design.js
+++ app/assets/javascripts/design.js
@@ -139,29 +139,4 @@ $(function () {
     $('#tooltip' + i).tooltip();
   }
 
-  /*
-  // Handled now by CSS transitions
-  setTimeout(function(){
-    $('.progress .bar').each(function() {
-        var me = $(this);
-        var perc = me.attr("data-percentage");
-
-        //TODO: left and right text handling
-
-        var current_perc = 0;
-
-        var progress = setInterval(function() {
-            if (current_perc>=perc) {
-                clearInterval(progress);
-            } else {
-                current_perc +=1;
-                me.css('width', (current_perc)+'%');
-            }
-
-           // me.text((current_perc)+'%');
-
-        }, 0);
-    });
-  },300);
-  */
 });
