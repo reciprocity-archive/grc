52963e3babf99aba35a2e01339de815dcac52419
Douglas Soo <dougsoo@google.com>
2012-08-29 11:07:51 -0700

    Get rid of unused experimental authorization code.

--- app/helpers/authorization_helper.rb
+++ app/helpers/authorization_helper.rb
@@ -11,37 +11,4 @@ module AuthorizationHelper
     #  filtered
     #end
   end
-
-  def self.included(model)
-    model.extend(ClassMethods)
-  end
-
-  module ClassMethods
-    def crud_acls(model)
-      model_name = model.table_name.singularize
-      access_control :crud_acl do
-        allow :superuser, :admin, :analyst
-
-        actions :new, :create do
-          allow 'create_' + model_name
-        end
-
-        actions :edit, :update do
-          allow 'update_' + model_name, :of => :control
-        end
-
-        actions :show, :tooltip do
-          allow 'read_' + model_name, :of => :control
-        end
-
-        actions :index do
-          allow 'read_' + model_name
-        end
-
-        actions :sections, :implemented_controls, :implementing_controls do
-          allow 'read_' + model_name, :of => :control
-        end
-      end
-    end
-  end
 end
