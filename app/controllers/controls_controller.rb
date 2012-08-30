249f7e6c16c3ff39308f201be47e554018512fb5
Dan Ring <danring@google.com>
2012-08-30 09:55:26 -0700

    Fix several issues
    - share 'control_params' helper between ControlsController
      and MappingController to fix mass-assignment issues
    - fix format of Quick Find's Biz Process tab and add tooltip
    - add 'data-form-target' attributes to several modal triggers
      to cause them to close properly

--- app/controllers/controls_controller.rb
+++ app/controllers/controls_controller.rb
@@ -100,31 +100,4 @@ class ControlsController < ApplicationController
     @controls.all.sort_by(&:slug_split_for_sort)
     render :action => 'controls', :layout => nil, :locals => { :controls => @controls, :prefix => 'Child of' }
   end
-
-  private
-
-    def control_params
-      control_params = params[:control] || {}
-      if control_params[:program_id]
-        # TODO: Validate the user has access to add controls to the program
-        control_params[:program] = Program.where(:id => control_params.delete(:program_id)).first
-      end
-      %w(type kind means).each do |field|
-        value_id = control_params.delete(field + '_id')
-        if value_id.present?
-          control_params[field] = Option.find(value_id)
-        end
-      end
-      %w(category).each do |field|
-        value_ids = control_params.delete(field + '_ids')
-        values = []
-        if value_ids.respond_to?(:each)
-          value_ids.each do |value_id|
-            values.push(Category.where(:id => value_id).first)
-          end
-          control_params[field.pluralize] = values
-        end
-      end
-      control_params
-    end
 end
