b4a13c2e2235a759b98b17bb7b1444956c0fc76a
Miron Cuperman <miron+code@google.com>
2012-09-18 13:34:12 -0700

    Sphinx config

--- extras/searchable_model.rb
+++ extras/searchable_model.rb
@@ -3,9 +3,20 @@ module SearchableModel
     model.extend(ClassMethods)
   end
 
+  def self.sphinx_servers
+    @@sphinx_servers ||= begin
+      servers = CMS_CONFIG['FULLTEXT']
+      (servers or '').split(',').map do |host_port|
+        { :host => host_port.split(':')[0],
+          :port => host_port.split(':')[1].to_i }
+      end
+    end
+    @@sphinx_servers
+  end
+
   module ClassMethods
     def fulltext_search(q)
-      if CMS_CONFIG["FULLTEXT"] == "1"
+      if !CMS_CONFIG["FULLTEXT"].blank?
         ids = search_for_ids(q).to_a
         where(:id => ids)
       else
