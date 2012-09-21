52d67f33a34792f314fc9991c3c4ddf400627412
Joseph Van Geffen <geffen@gmail.com>
2012-09-20 18:04:23 -0700

    Additional icons, favicon refresh issues fix, widgetbtn udpates, and more!

--- app/assets/javascripts/design.js
+++ app/assets/javascripts/design.js
@@ -225,8 +225,8 @@ jQuery(function ($) {
         $unassignedValue = parseInt($unassignedItems.html());
     
     $icon
-      .removeClass("icon-chevron-right")
-      .addClass("icon-ok")
+      .removeClass("grcicon-chevron-right")
+      .addClass("grcicon-check-green")
     $target
       .prepend('<li class="new-item"><div class="row-fluid"><div class="span6"><span class="company">' + $company + '</span><span class="name">'+ $name +'</span></div><div class="span3"><span class="label label-danger">No relationship</span></div><div class="span3 actions"><a href="#" class="btn btn-mini pull-right"><i class="icon-minus-sign"></i></a><a href="#" class="btn btn-mini pull-right"><i class="icon-pencil"></i></a></div></div><div class="row-fluid additional"><div class="span6"><label>Relationship <strong>PENDING</strong></label><select class="span12"><option>No relationship</option><option>Is Accountable</option><option>Is Responsible</option></select></div><div class="span3"><label>Start Date</label><input class="span12" type="text" placeholder="MM/DD/YYYY"></div><div class="span3"><label>Stop Date</label><input class="span12" type="text" placeholder="MM/DD/YYYY"></div></div></li>')
       .find("li.new-item").hide().fadeIn('slow').removeClass("new-item");  
@@ -368,9 +368,11 @@ jQuery(function($) {
 
   $('body').on('click', '#grcbutton-risk', function(e) {
     toggleRisk();
+    e.preventDefault();
   });
 
   $('body').on('click', '#grcbutton-governance', function(e) {
     toggleGovernance();
+    e.preventDefault();
   });
 });
