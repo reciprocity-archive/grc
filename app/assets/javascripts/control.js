e5cb9ac12d8358a5b08a48713df0642837e2cb0f
Bradley Momberger <brad@reciprocitylabs.com>
2012-11-28 18:26:26 -0800

    Demo code for showing CanJS populating the controls list on the controls page

---
+++ app/assets/javascripts/control.js
@@ -0,0 +1,13 @@
+//= require can.custom
+can.Model("CMS.Models.Control", {
+	storageName: 'controls'
+	, findAll : "GET /controls.json"
+    }
+    , {
+	
+    });
+/*
+can.Model.List("CMS.Models.Control.List", {
+
+    });
+    */
