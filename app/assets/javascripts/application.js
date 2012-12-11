200746b503273dcc3d40a4bff60d7462e46d1f39
Slobodan Kovacevic <basti@orangeiceberg.com>
2012-12-11 11:29:32 -0800

    Add wysihtml5 editor parser rules to support pasting tables.
    Implement formatable descriptions [story: #40582233]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,6 +6,7 @@
  *= require bootstrap/sticky-popover
  *= require bootstrap/modal-form
  *= require bootstrap/modal-ajax
+ *= require wysihtml5_parser_rules/advanced
  *= require wysihtml5-0.3.0_rc2
  *= require bootstrap-wysihtml5-0.0.2
  *= require_self
