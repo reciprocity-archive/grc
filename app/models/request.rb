d0260f192fa608fa905de2a2915aec5f8f3d4b8e
Slobodan Kovacevic <basti@orangeiceberg.com>
2013-01-09 12:51:14 +0100

    in request create dialog, make pbc_control_code required [story: #42183571]

--- app/models/request.rb
+++ app/models/request.rb
@@ -30,6 +30,7 @@ class Request < ActiveRecord::Base
 
   sanitize_attributes :pbc_control_desc, :request, :test, :notes
 
+  validates_presence_of :pbc_control_code
   validates :pbc_list,
     :presence => { :message => "needs a value" }
 
