672b93c104d7b6cdc6b0c25b1872d9c525732dc5
Bradley Momberger <brad@reciprocitylabs.com>
2013-01-11 14:52:31 -0800

    Story 41866453: Completed adding/removing/displaying of documents linked directly to Response (evidence documents)

--- app/models/response.rb
+++ app/models/response.rb
@@ -27,6 +27,33 @@ class Response < ActiveRecord::Base
   end
 
   def as_json_with_system(options={})
-    as_json(options.merge(:include => { :system => { :include => [:people, { :documents => { :methods => :link_url }} , :object_people, :object_documents] } , :object_people => { :include => :person } , :people => {}, :population_sample => { :include => { :population_document => { :methods => :link_url }, :sample_worksheet_document => { :methods => :link_url }, :sample_evidence_document => { :methods => :link_url }} }, :meetings => {} }))
+    as_json(options.merge(:include => { 
+      :system => { 
+        :include => [
+          :people, 
+          { 
+            :documents => { :methods => :link_url }
+          }, 
+          :object_people, 
+          :object_documents
+          ] 
+        }, 
+        :object_people => { :include => :person }, 
+        :people => {}, 
+        :population_sample => { 
+          :include => { 
+            :population_document => { :methods => :link_url }, 
+            :sample_worksheet_document => { :methods => :link_url }, 
+            :sample_evidence_document => { :methods => :link_url }
+          } 
+        }, 
+      :meetings => {}, 
+      :object_documents => {
+        :include => { :document => { :methods => :link_url }}
+      }, 
+      :documents => {
+        :methods => :link_url
+      }
+      }))
   end
 end
