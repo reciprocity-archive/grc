f3ad3d36ae98d680b792aea3a87beae876e94e41
Dan Ring <dfring@gmail.com>
2013-01-17 15:08:05 -0800

    Make migration not validate against new validations

--- db/migrate/20130117193334_remove_name_from_accounts.rb
+++ db/migrate/20130117193334_remove_name_from_accounts.rb
@@ -4,13 +4,13 @@ class RemoveNameFromAccounts < ActiveRecord::Migration
       person = Person.find_by_email(a.email)
       if person
         person.name = "#{a.name} #{a.surname}"
-        person.save!
+        person.save!(:validate => false)
       else
         a.person =
           Person.create({:email => a.email,
                         :name => "#{a.name} #{a.surname}"},
-                        :without_protection => true)
-        a.save!
+                        :without_protection => true, :validate => false)
+        a.save!(:validate => false)
       end
     end
     remove_column :accounts, [:name, :surname]
