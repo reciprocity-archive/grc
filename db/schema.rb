7edca8041a524ab33c47e5ce1e0533e192317c10
Dan Ring <dfring@gmail.com>
2013-01-21 18:49:22 -0800

    Change Relationship#*_type from Location to Facility

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130121223627) do
+ActiveRecord::Schema.define(:version => 20130122001958) do
 
   create_table "accounts", :force => true do |t|
     t.string   "username"
