9eca8ce622e4478043b5b839c3337a1dd46c61fd
Dan Ring <dfring@gmail.com>
2013-02-07 17:11:33 -0800

    Change Risk to use same Categories as Control [story:43643377]

--- app/models/risk.rb
+++ app/models/risk.rb
@@ -6,8 +6,6 @@ class Risk < ActiveRecord::Base
   include RelatedModel
   include SanitizableAttributes
 
-  CATEGORY_TYPE_ID = 103
-
   RATINGS = {
     1 => "Minimal",
     2 => "Moderate",
@@ -34,7 +32,7 @@ class Risk < ActiveRecord::Base
 
   # Categories
   has_many :categorizations, :as => :categorizable, :dependent => :destroy
-  has_many :categories, :through => :categorizations, :conditions => { :scope_id => Risk::CATEGORY_TYPE_ID }
+  has_many :categories, :through => :categorizations, :conditions => { :scope_id => Control::CATEGORY_TYPE_ID }
 
   is_versioned_ext
 
