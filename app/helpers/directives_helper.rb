91dc35f38caf2a8c569b8f44d5de144aa139500b
Dan Ring <dfring@gmail.com>
2013-02-18 15:30:55 -0800

    Re-add directive template fixes lost in the merge [story:41481113]

--- app/helpers/directives_helper.rb
+++ app/helpers/directives_helper.rb
@@ -2,10 +2,10 @@ module DirectivesHelper
   # Set up a directive and relevant stats for display
   def directive_stats(directive)
     stats = {}
-    stats[:sections_count] = Section.where(:program_id => directive.id).count()
-    stats[:sections_done_count] = Section.joins(:controls).where(:program_id => directive.id).count(:distinct => true)
-    stats[:sections_na_count] = Section.where(:program_id => directive.id, :na => true).count()
-    stats[:sections_undone_count] = Section.where(:program_id => directive.id).count() - stats[:sections_done_count] - stats[:sections_na_count]
+    stats[:sections_count] = Section.where(:directive_id => directive.id).count()
+    stats[:sections_done_count] = Section.joins(:controls).where(:directive_id => directive.id).count(:distinct => true)
+    stats[:sections_na_count] = Section.where(:directive_id => directive.id, :na => true).count()
+    stats[:sections_undone_count] = Section.where(:directive_id => directive.id).count() - stats[:sections_done_count] - stats[:sections_na_count]
 
     if stats[:sections_count] > 0
       stats[:sections_done_percentage] = 100.0 * stats[:sections_done_count] / stats[:sections_count]
@@ -13,7 +13,7 @@ module DirectivesHelper
       stats[:sections_undone_percentage] = 100.0 * stats[:sections_undone_count] / stats[:sections_count]
     end
 
-    controls = Control.joins(:sections).where(Section.arel_table[:program_id].eq(directive.id))
+    controls = Control.joins(:sections).where(Section.arel_table[:directive_id].eq(directive.id))
     stats[:controls_count] = controls.count(:distinct => true)
     stats[:controls_parented_count] = controls.where(Control.arel_table[:parent_id].not_eq(nil)).count(:distinct => true)
 
