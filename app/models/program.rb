cb8e5973f66f0e9bfc82e12b27080fdcc552d077
Dan Ring <dfring@gmail.com>
2013-02-18 18:03:23 -0800

    Add aggregated impact/scope widget to Program page [story:41481113]

--- app/models/program.rb
+++ app/models/program.rb
@@ -24,4 +24,39 @@ class Program < ActiveRecord::Base
   def display_name
     slug
   end
+
+  def impact_scope_for_directives
+    near_model = Directive
+    near_ids = directive_ids
+
+    data = near_model.valid_relationships.inject({}) do |memo, vr|
+      memo[vr[:related_model]] ||= {}
+      memo[vr[:related_model]][vr[:relationship_type]] ||= []
+
+      objects = memo[vr[:related_model]][vr[:relationship_type]]
+
+      far_model = vr[:related_model]
+
+      if vr[:related_model_endpoint] == :destination
+        Relationship.where(:source_type => near_model, :source_id => near_ids, :destination_type => far_model).all.each do |rel|
+          objects.push(rel.destination)
+        end
+      elsif vr[:related_model_endpoint] == :source
+        Relationship.where(:destination_type => near_model, :destination_id => near_ids, :source_type => far_model).all.each do |rel|
+          objects.push(rel.source)
+        end
+      end
+
+      memo
+    end
+
+    # De-duplicate objects in each relationship type's list
+    data.each do |model, rels|
+      rels.each do |rel_type_id, rel_objects|
+        rels[rel_type_id] = rel_objects.uniq
+      end
+    end
+
+    data
+  end
 end
