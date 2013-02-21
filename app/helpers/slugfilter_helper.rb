6eb3fe9d33b5a9ccec0db8f5c31013636bd12e9a
Dan Ring <dfring@gmail.com>
2013-02-20 20:02:11 -0800

    More fixes on program and directive pages [story:41430161]

--- app/helpers/slugfilter_helper.rb
+++ app/helpers/slugfilter_helper.rb
@@ -1,19 +1,24 @@
 module SlugfilterHelper
-  def walk_slug_tree(tree, depth=nil, &block)
+  def walk_slug_tree(tree, options=nil, &block)
+    options ||= {}
+    depth = options[:depth]
+
     capture_haml do
-      walk_slug_tree_children_helper(tree, nil, true, depth, &block)
+      walk_slug_tree_children_helper(tree, nil, true, depth, options, &block)
     end
   end
 
-  def walk_slug_tree_children_helper(tree, step=nil, odd=true, depth=nil, &block)
+  def walk_slug_tree_children_helper(tree, step=nil, odd=true, depth=nil, options=nil, &block)
+    options ||= {}
     children = tree.first_level_descendents_with_step
 
     children.each do |step, child|
-      walk_slug_tree_helper(child, step, odd, depth, &block)
+      walk_slug_tree_helper(child, step, odd, depth, options, &block)
     end
   end
 
-  def walk_slug_tree_helper(tree, step=nil, odd=true, depth=nil, &block)
+  def walk_slug_tree_helper(tree, step=nil, odd=true, depth=nil, options=nil, &block)
+    options ||= {}
     children = tree.first_level_descendents_with_step
 
     if tree.object && !depth.nil?
@@ -24,13 +29,13 @@ module SlugfilterHelper
     if tree.object || !children.empty?
       haml_tag("li", { :id => "content_#{tree.prefix}" }) do
         if tree.object
-          yield [tree.object, step]
+          yield [tree.object, step, children]
         end
         if !children.empty?
-          haml_tag("div.item-content.in") do
+          haml_tag("div.item-content", :class => options[:expanded] && 'in') do
             haml_tag("ul.tree-structure", { :id => "children_#{tree.prefix}" }) do
               children.each do |step, child|
-                walk_slug_tree_helper(child, step, odd, depth, &block)
+                walk_slug_tree_helper(child, step, odd, depth, options, &block)
               end
             end
           end
