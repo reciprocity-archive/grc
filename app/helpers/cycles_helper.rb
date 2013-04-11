d1f7704d6883c2e41f775e96c9c9ec6c13cf646d
bpfarmer <bpfarmer@gmail.com>
2013-04-11 14:49:33 -0400

    syntax error in cycles helper

--- app/helpers/cycles_helper.rb
+++ app/helpers/cycles_helper.rb
@@ -2,7 +2,7 @@ module CyclesHelper
 
   def generate_default_title_for_cycle(program=nil)
     default_title = (program.nil? ? "" : program.title) + " Audit"
-    duplicates = Cycle.where("title LIKE %?%", default_title).count
+    count = Cycle.where("title LIKE %?%", default_title).count
     default_title +=  count > 1 ? count.to_s : "" 
     return "#{Date.today.year} - " + default_title
   end
