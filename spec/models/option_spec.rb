9f43882089655ee88a4fe913fb42af2d5c309ded
bpfarmer <bpfarmer@gmail.com>
2013-04-12 14:57:55 -0400

    a non-lazy options test

--- spec/models/option_spec.rb
+++ spec/models/option_spec.rb
@@ -4,7 +4,7 @@ describe Option do
   
   it "should override appropriate roles" do
     overridden_role = Option::ROLES_OVERRIDE.map{|key, value| key}.first
-    Option.human_name(overridden_role).should_not == overridden_role.humanize.titleize
+    Option.human_name(overridden_role).should == Option::ROLES_OVERRIDE[overridden_role].humanize.titleize
   end
   
 end
