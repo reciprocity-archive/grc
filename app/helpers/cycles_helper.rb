7e0bfd4f36cb135187ab59c1dfbc535d0f685265
bpfarmer <bpfarmer@gmail.com>
2013-04-15 14:21:10 -0400

    using matches instead of like

--- app/helpers/cycles_helper.rb
+++ app/helpers/cycles_helper.rb
@@ -2,7 +2,7 @@ module CyclesHelper
 
   def generate_default_title_for_cycle(program=nil)
     default_title = (program.nil? ? "" : program.title + " ") + "Audit"
-    count = Cycle.where("title LIKE ?", "%#{default_title}%").count
+    count = Cycle.where(Cycle.arel_table[:title].matches("%#{default_title}%")).count
     default_title +=  count > 0 ? " " + (count + 1).to_s : ""
     return "#{Date.today.year} - " + default_title
   end
