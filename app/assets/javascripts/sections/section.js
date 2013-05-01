ac913ed8dce04bf38249889930589a7fa9dd66a7
Bradley Momberger <brad@reciprocitylabs.com>
2013-04-30 17:01:08 -0700

    Story 48945931: Corrected syntax error

--- app/assets/javascripts/sections/section.js
+++ app/assets/javascripts/sections/section.js
@@ -257,7 +257,7 @@ CMS.Models.Section("CMS.Models.SectionSlug", {
           while(list.length > 0) {
             can.$(list).each(function(i, v) {
               // find a pseudo-root whose parent wasn't in the returned sections
-              if(can.$(list).filter(function(j, c) { return c !== v && c.id === v.parent_id && c.directive_id === v.directive_id) }).length < 1) {
+              if(can.$(list).filter(function(j, c) { return c !== v && c.id === v.parent_id && c.directive_id === v.directive_id }).length < 1) {
                 current = v;
                 list.splice(i, 1); //remove current from list
                 return false;
