332f24024b784a6266ed2c3df7cf73244c60e7e7
Dan Ring <dfring@gmail.com>
2013-05-06 17:57:14 -0700

    Only check start date / end date if both have values [story:48144477]

--- extras/dated_model.rb
+++ extras/dated_model.rb
@@ -7,11 +7,15 @@ module DatedModel
   
   def stop_date_later_than_start_date
     if self.has_attribute? :stop_date
-      errors.add(:stop_date, 'Must be later than start date') unless
-       self.start_date < self.stop_date
+      if self.start_date.present? && self.stop_date.present?
+        errors.add(:stop_date, 'Must be later than start date') unless
+          self.start_date < self.stop_date
+      end
     elsif self.has_attribute? :end_at
-      errors.add(:end_at, 'Must be later than start date') unless
-        self.start_at < self.end_at
+      if self.start_at.present? && self.end_at.present?
+        errors.add(:end_at, 'Must be later than start date') unless
+          self.start_at < self.end_at
+      end
     end
   end
 end
