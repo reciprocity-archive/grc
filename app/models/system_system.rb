e517bbaa7d6b1802563cdda716a0c6bbd5c360f9
bpfarmer <bpfarmer@gmail.com>
2013-05-16 15:07:08 -0700

    Stories 50005129, 50004853: fixing delete system from link object dialog

--- app/models/system_system.rb
+++ app/models/system_system.rb
@@ -15,6 +15,7 @@ class SystemSystem < ActiveRecord::Base
   validate :does_not_link_to_self
   validate :does_not_create_cycles
   validate :does_not_duplicate_entry
+  validate :both_entries_exist
 
   def does_not_link_to_self
     if parent_id == child_id
@@ -33,6 +34,10 @@ class SystemSystem < ActiveRecord::Base
     if systems.count > 1 || (systems.first && systems.first.id != self.id)
       errors.add(:base, "Cannot create duplicate links")
     end
+  end
+  
+  def both_entries_exist
+    errors.add(:base, "System does not exist") unless System.where(:id => [parent_id, child_id]).count == 2
   end
 
   def has_cycle_to_parent?
