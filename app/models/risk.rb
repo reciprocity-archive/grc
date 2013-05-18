50fc2143813d46a359ce00e091f4c7030b23087a
bpfarmer <bpfarmer@gmail.com>
2013-05-17 17:48:28 -0700

    Converting risk likelihood rating to be more strict on import

--- app/models/risk.rb
+++ app/models/risk.rb
@@ -82,7 +82,7 @@ class Risk < ActiveRecord::Base
   def converted_likelihood_rating
     lr = read_attribute(:likelihood_rating) || 0.2
     if lr >= 1.0
-      (lr.to_f / 5.0) - 0.2
+      (lr.to_f / 5.0).round(1) - 0.2
     else
       lr
     end
