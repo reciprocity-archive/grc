3aca89a06e46689626d00945f1e4152f7a53c621
Dan Ring <dfring@gmail.com>
2013-05-22 11:40:29 -0700

    Map  role to new  role

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130514003757) do
+ActiveRecord::Schema.define(:version => 20130522175351) do
 
   create_table "accounts", :force => true do |t|
     t.string   "username"
